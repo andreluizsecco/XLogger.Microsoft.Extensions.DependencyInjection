fec5e01336729b10b9b5788209c42b953ed4314d
André Secco <andreluizsecco@yahoo.com.br>
2019-04-10 15:18:35 -0300

    Added documentation comments

--- src/XLogger.Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/XLogger.Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -6,9 +6,20 @@ namespace XLogger
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add XLogger to the logging pipeline.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add logging provider to.</param>
+        /// <param name="loggerHubOptions">The logger hub options and preferences.</param>
+        /// <returns>The collection services</returns>
         public static IServiceCollection AddXLogger(this IServiceCollection services, Action<ILoggerHub> loggerHubOptions) =>
             services.AddLogging(config => config.RegisterLoggerHub(loggerHubOptions));
 
+        /// <summary>
+        /// Clear logging providers and add XLogger to the logging pipeline.
+        /// </summary>
+        /// <param name="logging">The <see cref="ILoggingBuilder" /> to add logging provider to.</param>
+        /// <param name="loggerHubOptions">The logger hub options and preferences.</param>
         public static void RegisterLoggerHub(this ILoggingBuilder logging, Action<ILoggerHub> loggerHubOptions)
         {
             logging.ClearProviders();
